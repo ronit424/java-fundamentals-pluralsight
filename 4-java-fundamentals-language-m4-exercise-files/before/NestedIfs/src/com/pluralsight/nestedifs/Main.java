29ccb942d2a6fcfb4ce968f7c0060a1118567987
Ronit <ronitshah2004@gmail.com>
2020-04-17 10:24:03 +0545

    the main file

--- 4-java-fundamentals-language-m4-exercise-files/before/NestedIfs/src/com/pluralsight/nestedifs/Main.java
+++ 4-java-fundamentals-language-m4-exercise-files/before/NestedIfs/src/com/pluralsight/nestedifs/Main.java
@@ -6,6 +6,13 @@ public class Main {
         float students= 0.0f;
         float rooms = 4.0f;
 
+        if(students > 0.0f) {
+            if (rooms > 0.0f)
+                System.out.println(students / rooms);
+        }
+        else
+            System.out.println("No students");
+
         System.out.println();
         System.out.println("** end program **");
     }
