8fabc3968b50d0d183814bdabd9233bdd00c296b
Ronit <ronitshah2004@gmail.com>
2020-04-18 22:05:28 +0545

    program no 9

--- 9-java-fundamentals-language-m9-exercise-files/before/CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ 9-java-fundamentals-language-m9-exercise-files/before/CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -3,7 +3,7 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-//        useMathEquation();
+        useMathEquation();
 //        useCalculatorBase();
 
         String[] statements = {
